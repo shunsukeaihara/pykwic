514e211ef7a59361f6548d85221178c94b0dd837
Shunsuke Aihara <s.aihara@gmail.com>
2013-05-14 01:28:55 +0900

    ラッパーに関数を追加

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ext_modules = cythonize([
         Extension(
             'pyesary',
-            ["src/pyesary.pyx","src/esary.cpp","lib/esary.cpp"],
+            ["src/pyesary.pyx","lib/esary.cpp"],
             include_dirs=['lib','/opt/local/include','/usr/local/include'],
             library_dirs=['/opt/local/lib','/usr/local/lib'],
             libraries =['pthread','m','icui18n','icuuc','icudata'],
--- src/pyesary.pyx
+++ src/pyesary.pyx
@@ -1,10 +1,25 @@
 # distutils: language = c++
 from __future__ import unicode_literals
+from libcpp.string cimport string
+from libcpp.vector cimport vector
 cimport esary
 
-cdef class PyESary:
-
+cdef class ESary:
      cdef esary.ESary* _esary
 
      def __init__(self):
          self._esary = new esary.ESary()
+         print 'aa'
+
+     def add_line(self, uline):
+          cdef bytes line = uline.encode('utf8')
+          self._esary.addLine(line)
+
+     def build(self):
+         return self._esary.build()
+
+     def save(self,fname):
+         return self._esary.save(fname)
+
+     def load(self,fname):
+         return self._esary.load(fname)
