10b34144150335bc4aa44cd3e66c7f8faebeca6a
Shunsuke Aihara <s.aihara@gmail.com>
2013-05-25 03:40:48 +0900

    version 0.03

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sys.path.append('./src')
 setup(
     name="pykwic",
     description="KWIC Library for unicode text using Suffix Array.",
-    version="0.02",
+    version="0.03",
     long_description = open('README.rst').read(),
     ext_modules = cythonize([
         Extension(
