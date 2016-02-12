642927e6a8f1e5a088578eb8b3a7c41c31d2e29a
aihara <s.aihara@gmail.com>
2016-02-12 18:05:43 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,28 +2,25 @@
 from setuptools import setup
 from Cython.Build import cythonize
 from setuptools.extension import Extension
-import multiprocessing
 import sys
 sys.path.append('./src')
-
 
 setup(
     name="pykwic",
     description="KWIC Library for unicode text using Suffix Array.",
     version="0.03",
-    long_description = open('README.rst').read(),
-    ext_modules = cythonize([
+    long_description=open('README.rst').read(),
+    ext_modules=cythonize([
         Extension(
             'pykwic',
-            ["src/pykwic.pyx","lib/esary.cpp"],
-            include_dirs=['lib'],
-            ),
-        ],language="c++",
-                            ),
-    author = 'Shunsuke Aihara',
-    url = 'https://bitbucket.org/aihara/pykwic/',
-    license = "MIT License",
+            ["src/pykwic.pyx", "lib/esary.cpp"],
+            include_dirs=['lib']),
+    ],
+          language="c++"),
+    author='Shunsuke Aihara',
+    url='https://bitbucket.org/aihara/pykwic/',
+    license="MIT License",
     include_package_data=True,
     test_suite='nose.collector',
-    tests_require=['Nose','cython'],
-    )
+    tests_require=['Nose', 'cython'],
+)
